1f7208d89dadf9a4b180831c9c8ec2d117dae73e
Jawahar Balasubramani <jawaharbjkk@gmail.com>
2023-05-26 15:28:29 -0400

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-postcss','@storybook/addon-links', '@storybook/addon-essentials'],
+};
+// .storybook/main.js
